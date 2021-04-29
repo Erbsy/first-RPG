66e20e9310d93752868bd042404299055863bb03
manuel meisen <manuel.meisen@gmx.de>
2021-04-29 19:27:03 +0200

    Enemy Class

---
+++ Noob RPG/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noob_RPG
+{
+    public class Enemy
+    {
+        private int HP;
+        private int MaxHP;
+        private int AttackDamage;
+        private int Cooldown;
+        private int MaxCooldown;
+
+        private void DoDamage()
+        {
+
+        }
+
+        public void GetDamage(int damage)
+        {
+            HP = HP - damage;
+
+            if (HP <= 0)
+            {
+                Die();
+            }
+
+        }
+
+        public void DoTurn()
+        {
+            Cooldown -= 1;
+
+            if (Cooldown == 0)
+            {
+                DoDamage();
+                Cooldown = MaxCooldown;
+            }
+        }
+
+        private void Die()
+        {
+
+        }
+    }
+
+}
